a020365662567ea8a282aa065abcac2431ada473
Roman Kuzmich <87143625+psih2131@users.noreply.github.com>
2024-07-12 01:10:11 +0300

    add file progress loader

--- components/component__contact-form.vue
+++ components/component__contact-form.vue
@@ -1,5 +1,6 @@
 <template>
     <div class="contact-us-sec__form-wrapper form" >
+        <div class="form__white-cover-loader" :class="{'form__white-cover-loader_activ' : btnSendLoadingStatus}"></div>
         <!-- <div class="form__inp-wrapper">
             <p class="form__inp-title">{{ $t('contact_name') }}*</p>
             <input type="text" class="form__inp"
@@ -84,6 +85,12 @@
                   <svg width="20" height="20" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg">
                     <path d="M13.3333 4.99935V4.33268C13.3333 3.39926 13.3333 2.93255 13.1517 2.57603C12.9919 2.26243 12.7369 2.00746 12.4233 1.84767C12.0668 1.66602 11.6001 1.66602 10.6667 1.66602H9.33333C8.39991 1.66602 7.9332 1.66602 7.57668 1.84767C7.26308 2.00746 7.00811 2.26243 6.84832 2.57603C6.66667 2.93255 6.66667 3.39926 6.66667 4.33268V4.99935M8.33333 9.58268V13.7493M11.6667 9.58268V13.7493M2.5 4.99935H17.5M15.8333 4.99935V14.3327C15.8333 15.7328 15.8333 16.4329 15.5608 16.9677C15.3212 17.4381 14.9387 17.8205 14.4683 18.0602C13.9335 18.3327 13.2335 18.3327 11.8333 18.3327H8.16667C6.76654 18.3327 6.06647 18.3327 5.53169 18.0602C5.06129 17.8205 4.67883 17.4381 4.43915 16.9677C4.16667 16.4329 4.16667 15.7328 4.16667 14.3327V4.99935" stroke="#4B5565" stroke-width="1.66667" stroke-linecap="square"/>
                     </svg>
+                </div>
+              </div>
+
+              <div class="file-loaded-element__loader-wrapper">
+                <div class="file-loaded-element__loader" :class="{'file-loaded-element__loader_activ': btnSendLoadingStatus}">
+                  <span class="file-loaded-element__loader-progress-element"></span>
                 </div>
               </div>
             </div>
@@ -335,9 +342,6 @@ export default {
   methods: {
     //file load script
     fileCheck(event){
-      console.log(event.srcElement.files)
-      console.log(event)
-
       this.loadedFileList = Array.from(event.target.files)
 
       if(this.loadedFileList && this.loadedFileList.length > 0){
@@ -348,6 +352,7 @@ export default {
       }
     },
 
+    //remove loaded file
     removeFile(index){
       this.loadedFileList.splice(index, 1);
     },

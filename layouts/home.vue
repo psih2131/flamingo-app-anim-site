5585050e023be9bcd19994ef587d4bde59acb68b
wrz <igor.leonidov@jujumedia.co.uk>
2025-05-01 15:12:08 +0300

    add favic to main page;

--- layouts/home.vue
+++ layouts/home.vue
@@ -1,4 +1,20 @@
 <template>
+    <Head>
+        <Title>Flamingo | Country & State Travel Days Tracker</Title>
+        <Meta name="description" :content="`Count your international and stateside travel days with Flamingo, an iPhone app for global expats, permanent residents, overseas homeowners and travellers.`" />
+        <Meta name="og:title" :content="`Flamingo | Country & State Travel Days Tracker`" />
+        <Meta name="og:description" :content="`Count your international and stateside travel days with Flamingo, an iPhone app for global expats, permanent residents, overseas homeowners and travellers.`" />
+        <Meta name="og:image" :content="`/android-chrome-512x512.png`" />
+        <Meta name="og:url" :content="`https://next.flamingotracker.com/`" />
+        <Meta name="twitter:title" :content="`Flamingo | Country & State Travel Days Tracker`" />
+        <Meta name="twitter:description" :content="`Count your international and stateside travel days with Flamingo, an iPhone app for global expats, permanent residents, overseas homeowners and travellers.`" />
+        <Meta name="twitter:image" :content="`/android-chrome-512x512.png`" />
+        <Meta name="twitter:card" :content="'summary'" />
+        <Link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+        <Link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+        <Link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+        <Link rel="manifest" href="/site.webmanifest" />
+    </Head>
     <div class="app-wrapper">
         <component__header />
         

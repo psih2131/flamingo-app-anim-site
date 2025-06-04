0a20343d291125a5d9e16143f2d0d9f1b4478826
wrz <igor.leonidov@jujumedia.co.uk>
2025-06-04 10:37:30 +0300

    add logo test;

---
+++ pages/tests-logo.vue
@@ -0,0 +1,341 @@
+<template>
+    <main class="main legal-main">
+        <div class="container" style="position: relative;">
+            <!-- {{ angle }}
+            <div class="s1 rotating2" style="padding: 3.5px 202.5px 0 0; position: absolute; top: 128px;">
+                <svg style="width: 100%;height: auto;" width="1024" height="1024" viewBox="0 0 1024 1024" fill="none" xmlns="http://www.w3.org/2000/svg">
+                    <path d="M704.663 358C590.663 332.667 167.664 234 167.664 234C167.664 234 77.1636 674 481.163 776C481.163 776 537.163 787.5 578.663 789C620.163 790.5 650.163 790 681.163 784.5C712.163 779 821.165 743.832 853.161 638C894.882 500 818.663 383.333 704.663 358Z" fill="#96D0FF"/>
+                    <circle cx="703" cy="512" r="65" fill="#00CA9E"/>
+                </svg>
+            </div>
+
+            <div class="s1 rotating1" style="padding: 3.5px 202.5px 0 0; position: absolute; top: 128px;">
+                <svg style="width: 100%;height: auto;" width="1024" height="1024" viewBox="0 0 1024 1024" fill="none" xmlns="http://www.w3.org/2000/svg">
+                    <path d="M704.663 358C590.663 332.667 167.664 234 167.664 234C167.664 234 77.1636 674 481.163 776C481.163 776 537.163 787.5 578.663 789C620.163 790.5 650.163 790 681.163 784.5C712.163 779 821.165 743.832 853.161 638C894.882 500 818.663 383.333 704.663 358Z" fill="#D1E9E3"/>
+                    <circle cx="703" cy="512" r="65" fill="#00CA9E"/>
+                </svg>
+            </div>
+
+            <!-- <div class="s1 rotating" style="padding: 0.35% 27.15% 0 0;">
+            <div class="s1 rotating" style="padding: 3.5px 202.5px 0 0;">
+                <svg style="width: 100%;height: auto;" width="1024" height="1024" viewBox="0 0 1024 1024" fill="none" xmlns="http://www.w3.org/2000/svg">
+                    <path d="M704.663 358C590.663 332.667 167.664 234 167.664 234C167.664 234 77.1636 674 481.163 776C481.163 776 537.163 787.5 578.663 789C620.163 790.5 650.163 790 681.163 784.5C712.163 779 821.165 743.832 853.161 638C894.882 500 818.663 383.333 704.663 358Z" fill="#1F2A37"/>
+                    <circle cx="703" cy="512" r="65" fill="#00CA9E"/>
+                </svg>
+            </div>-->
+            
+            <div class="s1 rotating_" style="position: absolute; top: 128px;">
+                <svg style="width: 100%;height: auto;" width="1024" height="1024" viewBox="0 0 1024 1024" fill="none" xmlns="http://www.w3.org/2000/svg">
+                    <path fill-rule="evenodd" clip-rule="evenodd" d="M129.251 607C129.084 610.651 129 614.318 129 618C129 785.895 303.609 922 519 922C734.391 922 909 785.895 909 618C909 531.4 862.545 453.257 788 397.887V607H129.251Z" fill="#96D0FF"/>
+                    <circle cx="703" cy="512" r="65" fill="#00CA9E"/>
+                </svg>
+            </div>
+            <div class="s1 rotating2" style="position: absolute; top: 128px;">
+                <svg style="width: 100%;height: auto;" width="1024" height="1024" viewBox="0 0 1024 1024" fill="none" xmlns="http://www.w3.org/2000/svg">
+                    <path d="M772 383.5L341.001 101C341.001 101 282.5 221 274 358C265.501 495 341.001 606.5 341.001 606.5L772 383.5Z" fill="#D1E9E3"/>
+                    <circle cx="703" cy="512" r="65" fill="#00CA9E"/>
+                </svg>
+            </div>
+            <div class="s1 rotating1" style="position: absolute; top: 128px;">
+                <svg style="width: 100%;height: auto;" width="1024" height="1024" viewBox="0 0 1024 1024" fill="none" xmlns="http://www.w3.org/2000/svg">
+                    <path d="M704.663 358C590.663 332.667 167.664 234 167.664 234C167.664 234 77.1636 674 481.163 776C481.163 776 537.163 787.5 578.663 789C620.163 790.5 650.163 790 681.163 784.5C712.163 779 821.165 743.832 853.161 638C894.882 500 818.663 383.333 704.663 358Z" fill="#1F2A37"/>
+                    <circle cx="703" cy="512" r="65" fill="#00CA9E"/>
+                </svg>
+            </div>
+            <!-- <div class="s1 rotating" style="padding: 3.5px 202.5px 0 0;">
+                <svg style="width: 100%;height: auto;" width="1024" height="1024" viewBox="0 0 1024 1024" fill="none" xmlns="http://www.w3.org/2000/svg">
+                    <rect width="1024" height="1024" fill="white"/>
+                    <path fill-rule="evenodd" clip-rule="evenodd" d="M129.251 607C129.084 610.651 129 614.318 129 618C129 785.895 303.609 922 519 922C734.391 922 909 785.895 909 618C909 531.4 862.545 453.257 788 397.887V607H129.251Z" fill="#96D0FF"/>
+                    <path d="M772 383.5L341.001 101C341.001 101 282.5 221 274 358C265.501 495 341.001 606.5 341.001 606.5L772 383.5Z" fill="#D1E9E3"/>
+                    <path d="M704.663 358C590.663 332.667 167.664 234 167.664 234C167.664 234 77.1636 674 481.163 776C481.163 776 537.163 787.5 578.663 789C620.163 790.5 650.163 790 681.163 784.5C712.163 779 821.165 743.832 853.161 638C894.882 500 818.663 383.333 704.663 358Z" fill="#1F2A37"/>
+                    <circle cx="703" cy="512" r="65" fill="#00CA9E"/>
+                </svg>
+            </div> -->
+        </div>
+    </main>
+</template>
+
+
+<script setup>
+let angle = ref(0);
+onMounted(() => {
+    console.log('go');
+    // setInterval(() => {angle.value = angle.value + 1}, 100);
+});
+</script>
+
+<style>
+@-webkit-keyframes rotating /* Safari and Chrome */ {
+  from {
+    -webkit-transform: rotate(0deg);
+    -o-transform: rotate(0deg);
+    transform: rotate(0deg);
+  }
+  to {
+    -webkit-transform: rotate(-360deg);
+    -o-transform: rotate(-360deg);
+    transform: rotate(-360deg);
+  }
+}
+@keyframes rotating {
+  from {
+    -ms-transform: rotate(0deg);
+    -moz-transform: rotate(0deg);
+    -webkit-transform: rotate(0deg);
+    -o-transform: rotate(0deg);
+    transform: rotate(0deg);
+  }
+  to {
+    -ms-transform: rotate(-360deg);
+    -moz-transform: rotate(-360deg);
+    -webkit-transform: rotate(-360deg);
+    -o-transform: rotate(-360deg);
+    transform: rotate(-360deg);
+  }
+}
+
+@-webkit-keyframes tripping /* Safari and Chrome */ {
+    0% {
+    -webkit-transform: rotate(0deg);
+    -o-transform: rotate(0deg);
+    transform: rotate(0deg);
+  }
+  3% {
+    -webkit-transform: rotate(15deg);
+    -o-transform: rotate(15deg);
+    transform: rotate(15deg);
+  }
+  7% {
+    -webkit-transform: rotate(-15deg);
+    -o-transform: rotate(-15deg);
+    transform: rotate(-15deg);
+  }
+  10% {
+    -webkit-transform: rotate(0deg);
+    -o-transform: rotate(0deg);
+    transform: rotate(0deg);
+  }
+  100% {
+    -webkit-transform: rotate(0deg);
+    -o-transform: rotate(0deg);
+    transform: rotate(0deg);
+  }
+}
+@keyframes tripping {
+  0% {
+    -ms-transform: rotate(0deg);
+    -moz-transform: rotate(0deg);
+    -webkit-transform: rotate(0deg);
+    -o-transform: rotate(0deg);
+    transform: rotate(0deg);
+  }
+  3% {
+    -ms-transform: rotate(15deg);
+    -moz-transform: rotate(15deg);
+    -webkit-transform: rotate(15deg);
+    -o-transform: rotate(15deg);
+    transform: rotate(15deg);
+  }
+  7% {
+    -ms-transform: rotate(-15deg);
+    -moz-transform: rotate(-15deg);
+    -webkit-transform: rotate(-15deg);
+    -o-transform: rotate(-15deg);
+    transform: rotate(-15deg);
+  }
+  10% {
+    -ms-transform: rotate(0deg);
+    -moz-transform: rotate(0deg);
+    -webkit-transform: rotate(0deg);
+    -o-transform: rotate(0deg);
+    transform: rotate(0deg);
+  }
+  100% {
+    -ms-transform: rotate(0deg);
+    -moz-transform: rotate(0deg);
+    -webkit-transform: rotate(0deg);
+    -o-transform: rotate(0deg);
+    transform: rotate(0deg);
+  }
+}
+
+@-webkit-keyframes tripping /* Safari and Chrome */ {
+    0% {
+    -webkit-transform: rotate(0deg);
+    -o-transform: rotate(0deg);
+    transform: rotate(0deg);
+  }
+  3% {
+    -webkit-transform: rotate(15deg);
+    -o-transform: rotate(15deg);
+    transform: rotate(15deg);
+  }
+  7% {
+    -webkit-transform: rotate(-15deg);
+    -o-transform: rotate(-15deg);
+    transform: rotate(-15deg);
+  }
+  10% {
+    -webkit-transform: rotate(0deg);
+    -o-transform: rotate(0deg);
+    transform: rotate(0deg);
+  }
+  100% {
+    -webkit-transform: rotate(0deg);
+    -o-transform: rotate(0deg);
+    transform: rotate(0deg);
+  }
+}
+@keyframes tripping {
+  0% {
+    -ms-transform: rotate(0deg);
+    -moz-transform: rotate(0deg);
+    -webkit-transform: rotate(0deg);
+    -o-transform: rotate(0deg);
+    transform: rotate(0deg);
+  }
+  3% {
+    -ms-transform: rotate(15deg);
+    -moz-transform: rotate(15deg);
+    -webkit-transform: rotate(15deg);
+    -o-transform: rotate(15deg);
+    transform: rotate(15deg);
+  }
+  7% {
+    -ms-transform: rotate(-15deg);
+    -moz-transform: rotate(-15deg);
+    -webkit-transform: rotate(-15deg);
+    -o-transform: rotate(-15deg);
+    transform: rotate(-15deg);
+  }
+  10% {
+    -ms-transform: rotate(0deg);
+    -moz-transform: rotate(0deg);
+    -webkit-transform: rotate(0deg);
+    -o-transform: rotate(0deg);
+    transform: rotate(0deg);
+  }
+  100% {
+    -ms-transform: rotate(0deg);
+    -moz-transform: rotate(0deg);
+    -webkit-transform: rotate(0deg);
+    -o-transform: rotate(0deg);
+    transform: rotate(0deg);
+  }
+}
+
+@-webkit-keyframes tripping2 /* Safari and Chrome */ {
+    0% {
+    -webkit-transform: rotate(0deg);
+    -o-transform: rotate(0deg);
+    transform: rotate(0deg);
+  }
+  3% {
+    -webkit-transform: rotate(15deg);
+    -o-transform: rotate(15deg);
+    transform: rotate(15deg);
+  }
+  7% {
+    -webkit-transform: rotate(-15deg);
+    -o-transform: rotate(-15deg);
+    transform: rotate(-15deg);
+  }
+  10% {
+    -webkit-transform: rotate(0deg);
+    -o-transform: rotate(0deg);
+    transform: rotate(0deg);
+  }
+  100% {
+    -webkit-transform: rotate(0deg);
+    -o-transform: rotate(0deg);
+    transform: rotate(0deg);
+  }
+}
+@keyframes tripping2 {
+  0% {
+    -ms-transform: rotate(0deg);
+    -moz-transform: rotate(0deg);
+    -webkit-transform: rotate(0deg);
+    -o-transform: rotate(0deg);
+    transform: rotate(0deg);
+  }
+  3% {
+    -ms-transform: rotate(15deg);
+    -moz-transform: rotate(15deg);
+    -webkit-transform: rotate(15deg);
+    -o-transform: rotate(15deg);
+    transform: rotate(15deg);
+  }
+  7% {
+    -ms-transform: rotate(-15deg);
+    -moz-transform: rotate(-15deg);
+    -webkit-transform: rotate(-15deg);
+    -o-transform: rotate(-15deg);
+    transform: rotate(-15deg);
+  }
+  10% {
+    -ms-transform: rotate(0deg);
+    -moz-transform: rotate(0deg);
+    -webkit-transform: rotate(0deg);
+    -o-transform: rotate(0deg);
+    transform: rotate(0deg);
+  }
+  100% {
+    -ms-transform: rotate(0deg);
+    -moz-transform: rotate(0deg);
+    -webkit-transform: rotate(0deg);
+    -o-transform: rotate(0deg);
+    transform: rotate(0deg);
+  }
+}
+.rotating {
+  -webkit-animation: tripping 1.0s linear infinite;
+  -moz-animation: tripping 1.0s linear infinite;
+  -ms-animation: tripping 1.0s linear infinite;
+  -o-animation: tripping 1.0s linear infinite;
+  animation: tripping 1.0s linear infinite;
+}
+.rotating1 {
+  -webkit-animation: tripping 1.5s linear infinite;
+  -moz-animation: tripping 1.5s linear infinite;
+  -ms-animation: tripping 1.5s linear infinite;
+  -o-animation: tripping 1.5s linear infinite;
+  animation: tripping 1.5s linear infinite;
+}
+.rotating2 {
+  -webkit-animation: tripping 2.0s linear infinite;
+  -moz-animation: tripping 2.0s linear infinite;
+  -ms-animation: tripping 2.0s linear infinite;
+  -o-animation: tripping 2.0s linear infinite;
+  animation: tripping 2.0s linear infinite;
+}
+.legal-main .container {
+    border-bottom: none;
+}
+
+.s1 {
+    padding: 0.35% 27.15% 0 0;
+}
+/* @media only screen and (max-width: 2250px) { */
+    .main  {
+        min-height: 1000px;
+    }
+/* } */
+@media only screen and (max-width: 1250px) {
+    .s1 {
+        padding: 3.5px 202.5px 0 0;
+    }
+    .main  {
+        min-height: 700px;
+    }
+}
+
+@media only screen and (max-width: 750px) {
+    .s1 {
+        padding: 3.5px 94.5px 0 0;
+    }
+    .main  {
+        min-height: 500px;
+    }
+}
+</style>
